6f1804d18b219cb1ff036aed7d90de443f8f7fcf
ramysaxofonista <krlinis90@gmail.com>
2026-08-08 01:21:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,9 @@ function enviarWhatsApp() {
     return;
   }
 
-  let mensaje = `Hola RAMY, soy *${clientName}*. Esta es mi selección de canciones para el evento:`;
+  let mensaje = `Hola RAMY, soy *${clientName}*. Esta es mi selección de canciones para el evento:
+
+`;
   
   let i = 1;
   selectedCheckboxes.forEach(box => {
